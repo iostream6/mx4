2bf888fcf497460aae21baf8f715fcbc96ab99a4
Ilamah, Osho <oilamah@gmail.com>
2020-06-13 20:10:51 +0300

    Implemented Bar, Line and Doughnut chart.js chart components

---
+++ frontend/src/components/charts/BarChart.vue
@@ -0,0 +1,17 @@
+<!--
+ * 2020.06.13  - Created
+ *
+-->
+<script>
+import { Bar, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: ['chartData', "options"],
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
---
+++ frontend/src/components/charts/Doughnut.vue
@@ -0,0 +1,17 @@
+<!--
+ * 2020.06.13  - Created
+ *
+-->
+<script>
+import { Doughnut, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Doughnut,
+  mixins: [reactiveProp],
+  props: ["chartdata", "options"],
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  }
+};
+</script>
